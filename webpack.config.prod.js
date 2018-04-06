5c20295f44ac1c3c185ac7024e6c44b2ffdb538d
Marcin Bychawski <marcin.bychawski@dcx.bz>
2018-04-06 15:11:16 +0200

    Reorganize webpack configs

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,85 +1,97 @@
-const webpack = require("webpack");
-const autoprefixer = require("autoprefixer");
-const HTMLWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const autoprefixer = require('autoprefixer');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const path = require("path");
+const path = require('path');
 
 
 module.exports = {
-  mode: "production",
-  entry: [path.resolve("src/index.js")],
+  mode: 'production',
+  devtool: 'source-map',
+
+  entry: [path.resolve('src/index.js')],
+
+  resolve: {
+    modules: [
+      path.resolve('src'),
+      path.resolve('node_modules'),
+    ],
+    extensions: ['.js', '.jsx'],
+  },
+
+  output: {
+    path: path.resolve('dist'),
+    filename: '[name].[hash].js',
+    publicPath: '/',
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+    new HTMLWebpackPlugin({
+      template: path.resolve('public/index.html'),
+    }),
+  ],
+
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
             cacheDirectory: true,
-            plugins: ["react-hot-loader/babel"]
-          }
-        }
+          },
+        },
       },
       {
-        test: /\.scss$/,
+        test: /\.(scss|sass)$/,
         use: [
           MiniCssExtractPlugin.loader,
           {
-            loader: "css-loader",
-          },
-          {
-            loader: "sass-loader",
-          },
-          {
-            loader: "postcss-loader",
+            loader: 'css-loader',
             options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
               plugins: () => [
                 autoprefixer({
                   browsers: [
-                    ">1%",
-                    "last 4 versions",
-                    "Firefox ESR",
-                    "not ie < 10"
-                  ]
-                })
-              ]
-            }
-          }
-        ]
+                    '>1%',
+                    'last 4 versions',
+                    'Firefox ESR',
+                    'not ie < 10',
+                  ],
+                }),
+              ],
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
       },
-      {test: /\.(jpe?g|png|gif|ico)$/i, loader: 'file-loader?name=[name].[ext]'}
-    ]
+      {
+        test: /\.(jpe?g|png|gif|ico)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+            },
+          },
+        ],
+      },
+    ],
   },
-  resolve: {
-    extensions: [".js", ".jsx"]
-  },
-  output: {
-    path: path.resolve("dist"),
-    filename: "[name].[hash].js",
-    publicPath: "/"
-  },
-  //CSS can be extracted in one CSS file
-  optimization: {
-    splitChunks: {
-      cacheGroups: {
-        styles: {
-          test: /\.(css|sass|scss)$/,
-          name: 'styles',
-          chunks: 'all',
-          enforce: true
-        }
-      }
-    }
-  },
-  plugins: [
-    new CleanWebpackPlugin(['dist']),
-    new MiniCssExtractPlugin({
-      filename: "[name].css"
-    }),
-    new HTMLWebpackPlugin({
-      template: path.resolve("public/index.html")
-    })
-  ]
 };
