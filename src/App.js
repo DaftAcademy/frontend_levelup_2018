0d7a3f162c96d17c4158ef49ea43c75fb60fa0b6
Marcin Bychawski <marcin.bychawski@dcx.bz>
2018-04-06 15:10:58 +0200

    scss -> sass

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { hot } from 'react-hot-loader';
 import * as React from 'react';
 import Home from './view/Home';
 
-import "./styles/theme.scss";
+import './styles/theme.sass';
 
 class App extends React.Component { // eslint-disable-line react/prefer-stateless-function
   render() {
--- src/view/Home.js
+++ src/view/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import "./Home.scss";
+import './Home.sass';
 
 class Home extends React.Component {
   static propTypes = {
