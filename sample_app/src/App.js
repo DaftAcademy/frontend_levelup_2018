f6a97127b5768d9fea242a5e9f78908e3868ea02
Marcin Bychawski <marcin.bychawski@dcx.bz>
2018-04-18 12:27:57 +0200

    Make eslint happy again :)

--- sample_app/src/App.js
+++ sample_app/src/App.js
@@ -1,18 +1,17 @@
-import { hot } from "react-hot-loader";
-import * as React from "react";
-import Home from "./view/Home";
-import Menu from "./components/Menu";
+import { hot } from 'react-hot-loader';
+import * as React from 'react';
+import Home from './view/Home';
+import Menu from './components/Menu';
 
-import "./styles/theme.sass";
+import './styles/theme.sass';
 
 const MENU_ITEMS = [
-  { name: "Home" },
-  { name: "Search" },
-  { name: "Contact" }
-  ];
+  { name: 'Home' },
+  { name: 'Search' },
+  { name: 'Contact' },
+];
 
-class App extends React.Component {
-  // eslint-disable-line react/prefer-stateless-function
+class App extends React.Component { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
       <main>
--- sample_app/src/components/Menu.js
+++ sample_app/src/components/Menu.js
@@ -1,35 +1,41 @@
-import * as React from "react";
+import * as React from 'react';
+import PropTypes from 'prop-types';
 
 class Menu extends React.Component {
+  static propTypes = {
+    items: PropTypes.arrayOf(PropTypes.shape({
+      name: PropTypes.string,
+    })),
+  }
+
   state = {
-    selectedItem: undefined
+    selectedItem: undefined,
   };
 
-  onItemClick = event => {
+  onItemClick = (event) => {
     const { name: selectedItem } = event.currentTarget;
     this.setState({
-      selectedItem
+      selectedItem,
     });
   };
 
   render() {
-    console.info("selectedItem:", this.state.selectedItem);
+    console.info('selectedItem:', this.state.selectedItem);  // eslint-disable-line
     const { items } = this.props;
     const { selectedItem } = this.state;
     return (
       <div>
-        {items && items.length > 0
-          ? items.map(item =>
-              <button
-                key={item.name}
-                name={item.name}
-                onClick={this.onItemClick}
-                style={{ color: selectedItem === item.name ? "green" : "gray" }}
-              >
-                {item.name}
-              </button>
-            )
-          : "no data , sorry 🤡"}
+        {items && items.length > 0 ?
+          items.map(item => (
+            <button
+              key={item.name}
+              name={item.name}
+              onClick={this.onItemClick}
+              style={{ color: selectedItem === item.name ? 'green' : 'gray' }}
+            >
+              {item.name}
+            </button>
+          )) : 'no data , sorry 🤡'}
       </div>
     );
   }
